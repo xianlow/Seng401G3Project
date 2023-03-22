bbd76671eed26dc20c8dc71415912b5bc5da8166
Xian Wei Low <xwei.low@gmail.com>
2023-03-22 00:00:34 -0600

    SN-18 <Added API calling in engineering>

--- src/departments/Engineering.js
+++ src/departments/Engineering.js
@@ -1,24 +1,40 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Post from "../components/Post";
+import {Amplify, API} from 'aws-amplify';
+import awsconfig from "./aws-exports.js";
 
-function Engineering(){
+Amplify.configure(awsconfig);
+API.configure(awsconfig);
+const myAPI = "rhapsodyRest";
+const path = "/posts";
+
+
+  
+function Engineering(e){
+  const [input, setInput] = useState("")
+    const [posts, setPosts] = useState([])
     //fruits is just placeholder array for now
     const fruits = ["banana", "apple","kiwi","mango","pineapple"]; 
+    
+
+  //Function to fetch from our backend and update customers array
+  /*
+    let postsID = e.input
+    API.get(myAPI, path + "/" + postsID)
+       .then(response => {
+         console.log(response)
+         let newPosts = [...posts]
+         newPosts.push(response)
+         setPosts(newPosts)
+
+       })
+       .catch(error => {
+         console.log(error)
+       })
+      */
     //fetch text for each post then store in array which can then be mapped and formatted into a post
     return(
-    //     <div className="App" id="engg">
-    //         <div className="Left-side">
-    //             <Sidebar />
-    //         </div>
-    //         <div className='Right-side'>
-    //             <NewsFeed />
-    //             <Post depart = "Engineering" profilePic = {enggPfp} />
-    //             <Post depart = "Engineering" profilePic = {enggPfp} />
-    //             <Post depart = "Engineering" profilePic = {enggPfp} />
-    //         </div>
-    //   </div>
-        //map the post text to transform the array into post elements
-        fruits.map((fruits) => <Post postText={fruits}/>));
-}
+    fruits.map((fruits) => <Post postText={fruits}/>));
+      }
 
 export default Engineering
