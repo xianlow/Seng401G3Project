35c5b5aaea87944c74c9c9819fb48f21d0707555
jamgamm <gammuac344@gmail.com>
2023-03-24 00:58:57 -0600

    Combining frontend with backend Adding functionalities for add post button to connect to backend event sourcing. Improvement for displaying and getting all posts.

--- src/departments/Engineering.js
+++ src/departments/Engineering.js
@@ -1,24 +1,86 @@
-import React from "react"
-import Post from "../components/Post";
+import React, { Component } from "react";
+import testServices from "../services/testServices";
 
-function Engineering(){
-    //fruits is just placeholder array for now
-    const fruits = ["banana", "apple","kiwi","mango","pineapple"]; 
-    //fetch text for each post then store in array which can then be mapped and formatted into a post
-    return(
-    //     <div className="App" id="engg">
-    //         <div className="Left-side">
-    //             <Sidebar />
-    //         </div>
-    //         <div className='Right-side'>
-    //             <NewsFeed />
-    //             <Post depart = "Engineering" profilePic = {enggPfp} />
-    //             <Post depart = "Engineering" profilePic = {enggPfp} />
-    //             <Post depart = "Engineering" profilePic = {enggPfp} />
-    //         </div>
-    //   </div>
-        //map the post text to transform the array into post elements
-        fruits.map((fruits) => <Post postText={fruits}/>));
+import { Link } from "react-router-dom";
+const profilePic = "./images/ess-logo.png"
+export default class Engineering extends Component {
+  constructor(props) {
+    super(props);
+    this.retrieveTutorials = this.retrieveTutorials.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+
+    this.state = {
+      test: [],
+      currentTutorial: null,
+      currentIndex: -1,
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveTutorials();
+  }
+
+
+  retrieveTutorials() {
+    testServices.getAll()
+      .then(response => {
+        this.setState({
+          test: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveTutorials();
+    this.setState({
+      currentTutorial: null,
+      currentIndex: -1
+    });
+  }
+
+  removeAllTutorials() {
+    testServices.deleteAll()
+      .then(response => {
+        console.log(response.data);
+        this.refreshList();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+
+  render() {
+    const {test} = this.state;
+
+    return (
+
+          <div className="list-group">
+            {test &&
+              test.map((tutorial) => (
+                <div className="post">
+                    <div class="post_profile_pic">
+                    <img src={profilePic} alt="profile_pic"/>
+                    </div>
+                  <div class="post_content">
+                    <div class="post_text_Container">
+                        <div class="username">
+                            <h3>Engineering Department</h3>
+                        </div>
+                        <div class="post_description">
+                            <p>{tutorial.eventId}</p>
+                            <p>{tutorial.aggregateId}</p>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+          
+    );
+  }
 }
-
-export default Engineering
